e769df7aa91152bcafb0ad3eeb9bc23067221d23
david-amos <57191710+david-amos@users.noreply.github.com>
2021-02-15 08:18:23 -0600

    Added the model classes

---
+++ Pizzeria/Models/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Models
+{
+    
+    public class Pizza 
+    {
+        
+        public int PizzaID { get; set; }
+        
+        public string Name { get; set; }
+        public char Size { get; set; }
+        // 'S', 'M', or 'L'
+        
+    }
+}
